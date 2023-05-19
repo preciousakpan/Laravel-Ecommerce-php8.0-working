056d577e034def6f443a92639c24abe8242ef12d
Precious Akpan <67706629+preciousakpan@users.noreply.github.com>
2023-05-19 11:58:03 +0100

    Update testimonial.blade.php

--- src/resources/views/website/ecommerce/home_include/testimonial.blade.php
+++ src/resources/views/website/ecommerce/home_include/testimonial.blade.php
@@ -1,7 +1,7 @@
 <div class="testimonils-area box-shadow mtb-35 bg-fff">
     <div class="product-title home3-bg text-uppercase">
         <i class="fa fa-star-o icon home3-bg2"></i>
-        <h3>TESTIMONIALS</h3>
+        <h3>RECENT TESTIMONIALS</h3>
     </div>
     <div class="testmonial-active home2 right left-right-angle">
         <div class="single-testimonil clear">
@@ -12,7 +12,7 @@
                 <img src="{{ asset('contents/website') }}/img/test/2.jpg" alt="" />
             </div>
             <div class="test-info">
-                <h6>Kaji Hasibur Rahman</h6>
+                <h6>Precious Akpan</h6>
                 <span>Web Designer</span>
             </div>
         </div>
